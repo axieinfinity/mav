e3591b83b427bb4d047b4949602837c4bfe0b015
Trung Nguyen <me@ngthanhtrung.com>
2019-08-31 01:07:04 +0700

    Implement OS detection

--- src/util.rs
+++ src/util.rs
@@ -8,3 +8,21 @@ macro_rules! file_stem {
             .unwrap()
     };
 }
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub enum Os {
+    MacOs,
+    Linux,
+    Windows,
+    Other,
+}
+
+#[cfg(target_os = "macos")]
+pub const OS: Os = Os::MacOs;
+#[cfg(target_os = "linux")]
+pub const OS: Os = Os::Linux;
+#[cfg(target_os = "windows")]
+pub const OS: Os = Os::Windows;
+#[cfg(not(any(target_os = "macos", target_os = "linux", target_os = "windows")))]
+pub const OS: Os = Os::Other;
